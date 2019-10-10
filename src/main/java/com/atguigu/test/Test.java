6fbbc5c3dfa7f9258c516772a18acac6ecb93e29
qinhan <13849808656@163.com>
2019-10-10 15:09:51 +0800

    master分支修改文件并提交

--- src/main/java/com/atguigu/test/Test.java
+++ src/main/java/com/atguigu/test/Test.java
@@ -3,5 +3,8 @@ package com.atguigu.test;
 public class Test {
 	
 	public void test1() {}
+	public void testMaster() {
+		//主分支修改Test代码
+	}
 
 }
--- src/main/java/com/atguigu/test/Test2.java
+++ src/main/java/com/atguigu/test/Test2.java
@@ -1,5 +1,7 @@
 package com.atguigu.test;
 
 public class Test2 {
+	
+	//vbdsbubkcjb
 
 }
